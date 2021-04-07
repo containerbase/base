9b38625af08e829b3ef0ab92373a8228826df7f5
Michael Kriese <michael.kriese@visualon.de>
2021-04-07 11:36:22 +0200

    feat: install latest stable composer

--- src/usr/local/buildpack/tools/composer.sh
+++ src/usr/local/buildpack/tools/composer.sh
@@ -4,12 +4,15 @@ set -e
 
 
 check_command php
-check_semver ${TOOL_VERSION}
+
+if [[ "${TOOL_VERSION}" -ne "latest" ]]; then
+  check_semver ${TOOL_VERSION}
 
 
-if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
-  echo Invalid version: ${TOOL_VERSION}
-  exit 1
+  if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
+    echo Invalid version: ${TOOL_VERSION}
+    exit 1
+  fi
 fi
 
 tool_path=$(find_tool_path)
@@ -30,10 +33,10 @@ if [[ -z "${tool_path}" ]]; then
   # OpenShift
   chmod g+w ${base_path}
 
-
   BASE_URL="https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer"
+  VERS_ARG=$([[ "${TOOL_VERSION}" -eq "latest" ]] && echo "" || echo "--version=${TOOL_VERSION}")
 
-  curl -sSfL ${BASE_URL} | php -- --version=$TOOL_VERSION --install-dir=${tool_path}/bin --filename=composer
+  curl -sSfL ${BASE_URL} | php -- $VERS_ARG --install-dir=${tool_path}/bin --filename=composer
 
   update_env ${tool_path}
   shell_wrapper composer
