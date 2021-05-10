01e419681ca14026fe7d5d73b12df23ea7791a15
Michael Kriese <michael.kriese@visualon.de>
2021-05-10 16:16:35 +0200

    feat(java): use ppa with more versions

--- src/usr/local/buildpack/tools/java.sh
+++ src/usr/local/buildpack/tools/java.sh
@@ -5,6 +5,12 @@ set -e
 require_root
 check_semver $TOOL_VERSION
 
+
+echo "deb http://ppa.launchpad.net/openjdk-r/ppa/ubuntu ${VERSION_CODENAME} main" | tee -a /etc/apt/sources.list.d/java.list
+curl -sSL \
+  'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xDA1A4A13543B466853BAF164EB9B1D8886F44E2A' \
+  | apt-key add -
+
 apt_install openjdk-${MAJOR}-jdk-headless
 
 java -version
