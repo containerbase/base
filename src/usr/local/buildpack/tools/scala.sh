542258b9e2bb24669e40ca836196108d278d496d
Michael Kriese <michael.kriese@visualon.de>
2021-05-12 08:58:43 +0200

    feat(java): add scala installer

---
+++ src/usr/local/buildpack/tools/scala.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+check_semver ${TOOL_VERSION}
+
+
+if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
+  echo Invalid version: ${TOOL_VERSION}
+  exit 1
+fi
+
+tool_path=$(find_tool_path)
+
+function update_env () {
+  reset_tool_env
+  export_tool_path "${1}/bin"
+}
+
+if [[ -z "${tool_path}" ]]; then
+  INSTALL_DIR=$(get_install_dir)
+  base_path=${INSTALL_DIR}/${TOOL_NAME}
+  tool_path=${base_path}/${TOOL_VERSION}
+
+  mkdir -p ${base_path}
+
+  file=/tmp/${TOOL_NAME}.tgz
+
+  URL='https://downloads.lightbend.com'
+
+  curl -sSfLo ${file} ${URL}/${TOOL_NAME}/${TOOL_VERSION}/${TOOL_NAME}-${TOOL_VERSION}.tgz
+  tar -C ${base_path} -xf ${file}
+  rm ${file}
+
+  update_env ${tool_path}
+
+else
+  echo "Already installed, resetting env"
+  update_env ${tool_path}
+fi
+
+scala --version
