75c716fc441b80ea98563e55d86ca6ec43e57388
Michael Kriese <michael.kriese@visualon.de>
2021-05-17 16:49:55 +0200

    fix: do not add apt sources twice

--- src/usr/local/buildpack/tools/java.sh
+++ src/usr/local/buildpack/tools/java.sh
@@ -7,7 +7,7 @@ check_semver $TOOL_VERSION
 
 VERSION_CODENAME=$(. /etc/os-release && echo ${VERSION_CODENAME})
 
-echo "deb http://ppa.launchpad.net/openjdk-r/ppa/ubuntu ${VERSION_CODENAME} main" | tee -a /etc/apt/sources.list.d/java.list
+echo "deb http://ppa.launchpad.net/openjdk-r/ppa/ubuntu ${VERSION_CODENAME} main" | tee /etc/apt/sources.list.d/java.list
 curl -sSL \
   'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xDA1A4A13543B466853BAF164EB9B1D8886F44E2A' \
   | apt-key add -
