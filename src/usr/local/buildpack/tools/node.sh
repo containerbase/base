b18edd71689048c4621b307e7fb6828e381cb8d8
Michael Kriese <michael.kriese@visualon.de>
2021-07-15 17:06:34 +0200

    feat(node): use openssl ca certificates

--- src/usr/local/buildpack/tools/node.sh
+++ src/usr/local/buildpack/tools/node.sh
@@ -47,5 +47,8 @@ chmod -R g+w $NPM_CONFIG_PREFIX
 export_env NPM_CONFIG_PREFIX $NPM_CONFIG_PREFIX
 export_path "\$NPM_CONFIG_PREFIX/bin"
 
+# use openssl certificates
+export_env NODE_OPTIONS "--use-openssl-ca"
+
 shell_wrapper node
 shell_wrapper npm
