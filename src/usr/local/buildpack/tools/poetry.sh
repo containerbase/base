791f37d2ed484d395a343af53f45831d6f9ba231
Michael Kriese <michael.kriese@visualon.de>
2021-08-30 13:56:22 +0200

    fix: add poetry workaround
    fixes #123

--- src/usr/local/buildpack/tools/poetry.sh
+++ src/usr/local/buildpack/tools/poetry.sh
@@ -29,8 +29,14 @@ if [[ -z "${tool_path}" ]]; then
   curl -sSL $POETRY_URL | python - --version ${TOOL_VERSION}
   unset POETRY_HOME
 
-  # fix execute for all [#150]
+  # fix execute for all renovatebot/docker-buildpack#150
   chmod +x ${tool_path}/bin/poetry
+
+  # fix uid/ fid #124
+  if [[ $UID -eq 0 ]]; then
+    [ -f "${tool_path}/lib/poetry/_vendor/py2.7/backports/entry_points_selectable.py" ] \
+      && chown 0:0 ${tool_path}/lib/poetry/_vendor/py2.7/backports/entry_points_selectable.py
+  fi
 fi
 
 update_env ${tool_path}
