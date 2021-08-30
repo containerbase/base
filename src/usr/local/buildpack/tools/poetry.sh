63916bccc31b032eebbf5eb1b09b1368502b92cc
Michael Kriese <michael.kriese@visualon.de>
2021-08-30 12:38:44 +0200

    feat(poetry): use new installer

--- src/usr/local/buildpack/tools/poetry.sh
+++ src/usr/local/buildpack/tools/poetry.sh
@@ -10,7 +10,12 @@ if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
   exit 1
 fi
 
-POETRY_URL=https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py
+# if [[ ${MAJOR} -gt 1 || (${MAJOR} -eq 1 && ${MINOR} -gt 1) || (${MAJOR} -eq 1 && ${MINOR} -eq 1 && ${PATCH} -gt 6) ]]; then
+#   POETRY_URL=https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py
+# else
+  POETRY_URL=https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py
+# fi
+
 tool_path=$(find_tool_path)
 
 function update_env () {
