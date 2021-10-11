9eb7b0fe7296a2f00eb209b3cb47b3059c9f479f
Michael Kriese <michael.kriese@visualon.de>
2021-10-11 13:36:45 +0200

    ci: update apt sources before install

--- tools/prepare-proxy.js
+++ tools/prepare-proxy.js
@@ -2,6 +2,7 @@ import shell from 'shelljs';
 
 shell.echo(`Preparing squid-deb-proxy`);
 
+shell.exec('apt-get -qq update');
 shell.exec('apt-get install -y squid-deb-proxy');
 shell
   .cat('./tools/buildpack.acl')
