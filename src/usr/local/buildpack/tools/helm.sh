eb78b2e5da8678c7d2918ceb3c70f80b325cf7b7
Rhys Arkins <rhys@arkins.net>
2021-11-10 15:31:06 +0100

    update install

--- src/usr/local/buildpack/tools/helm.sh
+++ src/usr/local/buildpack/tools/helm.sh
@@ -10,17 +10,33 @@ if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
   exit 1
 fi
 
-if [[ -d "/usr/local/${TOOL_NAME}/${TOOL_VERSION}" ]]; then
-  echo "Skipping, already installed"
-  exit 0
+tool_path=$(find_tool_path)
+
+function update_env () {
+  reset_tool_env
+  export_tool_env MAVEN_HOME "${1}"
+  export_tool_path "${1}/bin"
+}
+
+if [[ -z "${tool_path}" ]]; then
+  INSTALL_DIR=$(get_install_dir)
+  base_path=${INSTALL_DIR}/${TOOL_NAME}
+  tool_path=${base_path}/${TOOL_VERSION}
+
+  mkdir -p ${tool_path}
+
+  file=/tmp/${TOOL_NAME}.tgz
+
+  curl -sSfLo ${file} https://get.helm.sh/helm-v${TOOL_VERSION}-linux-amd64.tar.gz
+  tar --strip 1 -C ${tool_path} -xf ${file}
+  rm ${file}
+
+  update_env ${tool_path}
+  shell_wrapper mvn
+else
+  echo "Already installed, resetting env"
+  update_env ${tool_path}
 fi
-
-mkdir -p /usr/local/${TOOL_NAME}/${TOOL_VERSION}
-curl -sSL https://get.helm.sh/helm-v${TOOL_VERSION}-linux-amd64.tar.gz --output helm.tgz
-tar --strip 1 -C /usr/local/${TOOL_NAME}/${TOOL_VERSION} -xzf helm.tgz
-rm helm.tgz
-
-export_path "/usr/local/${TOOL_NAME}/${TOOL_VERSION}"
 
 helm version
 
