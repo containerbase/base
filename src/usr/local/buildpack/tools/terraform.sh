895f2fa7278ad7d9856cbc1a4314d2b65f6baf05
Rhys Arkins <rhys@arkins.net>
2021-11-10 15:58:30 +0100

    fix: terraform non-root install

--- src/usr/local/buildpack/tools/terraform.sh
+++ src/usr/local/buildpack/tools/terraform.sh
@@ -2,23 +2,40 @@
 
 set -e
 
-check_semver ${TERRAFORM_VERSION}
+check_semver ${TOOL_VERSION}
+
 
 if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
-  echo Invalid version: ${TERRAFORM_VERSION}
+  echo Invalid version: ${TOOL_VERSION}
   exit 1
 fi
 
-if [[ -d "/usr/local/bin/${TOOL_NAME}" ]]; then
-  echo "Skipping, already installed"
-  exit 0
+tool_path=$(find_tool_path)
+
+function update_env () {
+  reset_tool_env
+  export_tool_path "${1}"
+}
+
+if [[ -z "${tool_path}" ]]; then
+  INSTALL_DIR=$(get_install_dir)
+  base_path=${INSTALL_DIR}/${TOOL_NAME}
+  tool_path=${base_path}/${TOOL_VERSION}
+
+  mkdir -p ${tool_path}
+
+  file=/tmp/${TOOL_NAME}.tgz
+
+  DISTRO=linux_amd64
+  curl -sSfLo ${file} https://releases.hashicorp.com/terraform/${TOOL_VERSION}/terraform_${TOOL_VERSION}_${DISTRO}.zip
+  unzip -q -d ${tool_path} ${file}
+  rm ${file}
+
+  update_env ${tool_path}
+  shell_wrapper terraform
+else
+  echo "Already installed, resetting env"
+  update_env ${tool_path}
 fi
-
-DISTRO=linux_amd64
-URL=https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${DISTRO}.zip
-
-curl -sL $URL -o tmp.zip
-unzip -q -d /usr/local/bin/ tmp.zip
-rm tmp.zip
 
 terraform version
