ee3713deec9f02c238c2ff23dc722095a40de848
Michael Kriese <michael.kriese@visualon.de>
2021-12-07 17:40:10 +0100

    fix: wrong powershell install

--- src/usr/local/buildpack/tools/powershell.sh
+++ src/usr/local/buildpack/tools/powershell.sh
@@ -10,25 +10,28 @@ if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
   exit 1
 fi
 
-if [[ -d "/usr/local/${TOOL_NAME}/${TOOL_VERSION}" ]]; then
-  echo "Skipping, already installed"
-  exit 0
+
+tool_path=$(find_tool_path)
+
+if [[ -z "${tool_path}" ]]; then
+  INSTALL_DIR=$(get_install_dir)
+  base_path=${INSTALL_DIR}/${TOOL_NAME}
+  tool_path=${base_path}/${TOOL_VERSION}
+
+  VERSION_CODENAME=$(. /etc/os-release && echo ${VERSION_CODENAME})
+
+  case "$VERSION_CODENAME" in
+    "bionic") apt_install libc6 libgcc1 libgssapi-krb5-2 libicu60 libssl1.1 libstdc++6 zlib1g;;
+    "focal") apt_install libc6 libgcc1 libgssapi-krb5-2 libicu66 libssl1.1 libstdc++6 zlib1g;;
+  esac
+
+  mkdir -p $tool_path/bin
+  curl -sSL https://github.com/PowerShell/PowerShell/releases/download/v${TOOL_VERSION}/powershell-${TOOL_VERSION}-linux-x64.tar.gz --output ${TOOL_NAME}.tgz
+  tar --strip 1 -C $tool_path/bin -xzf ${TOOL_NAME}.tgz
+  rm ${TOOL_NAME}.tgz
 fi
 
-VERSION_CODENAME=$(. /etc/os-release && echo ${VERSION_CODENAME})
-
-case "$VERSION_CODENAME" in
-  "bionic") apt_install libc6 libgcc1 libgssapi-krb5-2 libicu60 libssl1.1 libstdc++6 zlib1g;;
-  "focal") apt_install libc6 libgcc1 libgssapi-krb5-2 libicu66 libssl1.1 libstdc++6 zlib1g;;
-esac
-
-mkdir -p /usr/local/${TOOL_NAME}/${TOOL_VERSION}
-curl -sSL https://github.com/PowerShell/PowerShell/releases/download/v${TOOL_VERSION}/powershell-${TOOL_VERSION}-linux-x64.tar.gz --output ${TOOL_NAME}.tgz
-tar --strip 1 -C /usr/local/${TOOL_NAME}/${TOOL_VERSION} -xzf ${TOOL_NAME}.tgz
-rm ${TOOL_NAME}.tgz
-
-
-PATH="${1}/bin:${PATH}"
+PATH="${tool_path}/bin:${PATH}"
 link_wrapper pwsh
 
 pwsh -Version
