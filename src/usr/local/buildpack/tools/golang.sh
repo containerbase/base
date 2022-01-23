8d1502b6498d8511c8164064e6fa1b1e132803da
Chumper <git-hub@nilsplaschke.de>
2022-01-23 20:05:31 +0100

    feat: add script to make tools runtime installable

--- src/usr/local/buildpack/tools/golang.sh
+++ src/usr/local/buildpack/tools/golang.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-require_root
-check_semver ${TOOL_VERSION}
+check_semver "${TOOL_VERSION}"
 
 
 if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
-  echo Invalid version: ${TOOL_VERSION}
+  echo Invalid version: "${TOOL_VERSION}"
   exit 1
 fi
 
 
-base_path=/usr/local/buildpack/go
-tool_path=${base_path}/${TOOL_VERSION}
+tool_path=$(find_versioned_tool_path)
 
-if [[ ! -d "$tool_path" ]]; then
+if [[ -z "$tool_path" ]]; then
+
+  tool_path=$(create_versioned_tool_path)
 
   # fix version
   GOLANG_FILE_VERSION=${TOOL_VERSION}
@@ -23,28 +23,24 @@ if [[ ! -d "$tool_path" ]]; then
     GOLANG_FILE_VERSION="${MAJOR}.${MINOR}"
   fi
 
-  # go suggests: git svn bzr mercurial
-  apt_install bzr mercurial
-
-  mkdir -p $tool_path
-  curl -sSL https://dl.google.com/go/go${GOLANG_FILE_VERSION}.linux-amd64.tar.gz --output go.tgz
-  tar --strip 1 -C $tool_path -xzf go.tgz
+  curl -sSL "https://dl.google.com/go/go${GOLANG_FILE_VERSION}.linux-amd64.tar.gz" --output go.tgz
+  tar --strip 1 -C "$tool_path" -xzf go.tgz
   rm go.tgz
 
   if [[ ! -d "${GOPATH}" ]]; then
-    export_env GOPATH "/go"
-    export_env CGO_ENABLED 0
-    export_env GOSUMDB off
-    export_path "\$GOPATH/bin"
+    export_tool_env GOPATH "${tool_path}/go"
+    export_tool_env CGO_ENABLED 0
+    export_tool_env GOSUMDB off
+    export_tool_path "\$GOPATH/bin"
 
     mkdir -p "$GOPATH/src" "$GOPATH/bin" "$GOPATH/pkg"
 
-    chown -R ${USER_ID} $GOPATH
-    chmod -R g+w $GOPATH
+    chown -R "${USER_ID}" "$GOPATH"
+    chmod -R g+w "$GOPATH"
   fi
 fi
 
-link_wrapper go $tool_path/bin
+link_wrapper go "$tool_path/bin"
 
 go version
 go env
