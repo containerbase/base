ab221da646c1eb36fab229f6c6008f720ed5f516
Chumper <git-hub@nilsplaschke.de>
2022-01-23 19:07:33 +0100

    fix: use actual functions instead of their names

--- src/usr/local/buildpack/util.sh
+++ src/usr/local/buildpack/util.sh
@@ -238,17 +238,29 @@ function get_install_dir () {
 }
 
 function find_tool_path () {
-  install_dir=get_install_dir
+  install_dir=$(get_install_dir)
   if [[ -d "${install_dir}/${TOOL_NAME}" ]]; then
     echo "${install_dir}/${TOOL_NAME}"
   fi
 }
 
 function find_versioned_tool_path () {
-  tool_dir=find_tool_path
+  tool_dir=$(find_tool_path)
   if [[ -d "${tool_dir}/${TOOL_VERSION}" ]]; then
     echo "${tool_dir}/${TOOL_VERSION}"
   fi
+}
+
+function create_tool_path () {
+  install_dir=$(get_install_dir)
+  mkdir -p "${install_dir}/${TOOL_NAME}"
+  echo "${install_dir}/${TOOL_NAME}"
+}
+
+function create_versioned_tool_path () {
+  install_dir=$(get_install_dir)
+  mkdir -p "${install_dir}/${TOOL_NAME}/${TOOL_VERSION}"
+  echo "${install_dir}/${TOOL_NAME}/${TOOL_VERSION}"
 }
 
 
--- src/usr/local/buildpack/utils/ruby.sh
+++ src/usr/local/buildpack/utils/ruby.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 function gem_install() {
-  tool_path=$(find_versioned_tool_path)
+  tool_path=$(find_tool_path)
 
   if [[ -z "${tool_path}" ]]; then
-    INSTALL_DIR=$(get_install_dir)
-    base_path=${INSTALL_DIR}/${TOOL_NAME}
-    tool_path=${base_path}/${TOOL_VERSION}
+    tool_path=$(create_versioned_tool_path)
+    mkdir -p "${tool_path}"
 
-    mkdir -p ${tool_path}
-
-    GEM_HOME=$tool_path gem install  --bindir $tool_path/bin ${TOOL_NAME} -v ${TOOL_VERSION}
+    GEM_HOME=$tool_path gem install  --bindir "$tool_path"/bin "${TOOL_NAME}" -v "${TOOL_VERSION}"
 
   # TODO: clear gem cache
   fi
@@ -18,22 +15,27 @@ function gem_install() {
 
 
 function gem_link_wrapper() {
-  link_wrapper ${1:-$TOOL_NAME} $tool_path/bin
+  link_wrapper "${1:-$TOOL_NAME}" "$tool_path/bin"
 }
 
 function gem_shell_wrapper () {
-  local install_dir=$(get_install_dir)
-  local FILE="${install_dir}/bin/${1:-$TOOL_NAME}"
+  local install_dir
+  local tool_file
+  local ruby_version
+
+  install_dir=$(get_install_dir)
+  tool_file="${install_dir}/bin/${1:-$TOOL_NAME}"
   # TODO: make generic
-  local ruby_version=$(cat /usr/local/ruby/.version)
+  ruby_version=$(cat /usr/local/ruby/.version)
+
   tool_path=$(find_versioned_tool_path)
-  check_command ${tool_path}/bin/${1:-$TOOL_NAME}
-  cat > $FILE <<- EOM
+  check_command "${tool_path}/bin/${1:-$TOOL_NAME}"
+  cat > "$tool_file" <<- EOM
 #!/bin/bash
 
 export GEM_PATH=${tool_path}:/usr/local/ruby/${ruby_version}/lib/ruby/gems/${ruby_version} PATH=${tool_path}/bin:\$PATH
 
 ${1:-$TOOL_NAME} "\$@"
 EOM
-  chmod +x $FILE
+  chmod +x "$tool_file"
 }
