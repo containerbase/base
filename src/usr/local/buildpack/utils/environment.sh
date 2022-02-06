8d1b08dc73f5c449785366f38cab0671cc997a2f
Chumper <Chumper@users.noreply.github.com>
2022-02-06 17:11:47 +0100

    fix: evaluate root id later

--- src/usr/local/buildpack/utils/environment.sh
+++ src/usr/local/buildpack/utils/environment.sh
@@ -77,7 +77,7 @@ function setup_env_files () {
 export BUILDPACK=1 USER_NAME="${USER_NAME}" USER_ID="${USER_ID}" USER_HOME="${USER_HOME}"
 
 # openshift override unknown user home
-if [[ $EUID -ne 0 ]]; then
+if [[ "\${EUID}" -ne 0 ]]; then
   export HOME="\${USER_HOME}"
 fi
 
