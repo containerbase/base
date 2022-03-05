c03d707f8ea81d12b80619a0cb531eaeda469283
Nils Plaschke <Chumper@users.noreply.github.com>
2022-03-05 23:26:09 +0100

    feat: migrate Helm to a v2 tool (#300)
    * refactor: migrate Helm to a v2 tool
    
    * chore: test all bats files
    
    * chore: set PATH globally
    
    * chore: set PATH globally in distro test
    
    * chore: remove default functions
    
    * Update Dockerfile
    
    * Update test/Dockerfile.bionic
    
    * feat: add more helm tests
    
    * chore: remove optional function
    
    * chore: use renovating helm version
    
    * chore: remove non working Renovate versioning
    
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

---
+++ src/usr/local/buildpack/tools/v2/helm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function install_tool () {
+  local versioned_tool_path
+  versioned_tool_path=$(create_versioned_tool_path)
+
+  create_folder "${versioned_tool_path}/bin"
+
+  local file
+  file=$(get_from_url "https://get.helm.sh/helm-v${TOOL_VERSION}-linux-amd64.tar.gz")
+  tar --strip 1 -C "${versioned_tool_path}/bin" -xf "${file}"
+}
+
+function link_tool () {
+  local versioned_tool_path
+  versioned_tool_path=$(find_versioned_tool_path)
+
+  link_wrapper "${TOOL_NAME}" "${versioned_tool_path}/bin"
+  helm version
+}
