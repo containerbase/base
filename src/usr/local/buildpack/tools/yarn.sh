4108760e9314b5cbb94872f18ad98da3fc764a15
Michael Kriese <michael.kriese@visualon.de>
2022-03-25 13:38:30 +0100

    feat(node): refactor prefix handling

--- src/usr/local/buildpack/tools/yarn.sh
+++ src/usr/local/buildpack/tools/yarn.sh
@@ -4,22 +4,16 @@ set -e
 
 check_command node
 
+# shellcheck source=/dev/null
+. /usr/local/buildpack/utils/node.sh
+
 tool_path=$(find_versioned_tool_path)
 
 if [[ -z "${tool_path}" ]]; then
-  tool_path="$(create_versioned_tool_path)"
-
-  temp_folder=$(mktemp -u)
-  mkdir -p "${temp_folder}"
-
-  npm cache clean --force
-  NPM_CONFIG_PREFIX=$tool_path npm install --cache "${temp_folder}" -g "${TOOL_NAME}@${TOOL_VERSION}"
-
-  # Clean download cache
-  NPM_CONFIG_PREFIX=$tool_path npm cache clean --force
-  # Clean node-gyp cache
-  rm -rf "$HOME/.cache" "${temp_folder}"
-  chmod -R 775 "${HOME}/.npm" || true
+  npm_init
+  npm_install
+  tool_path=$(find_versioned_tool_path)
+  npm_clean
 fi
 
 link_wrapper "${TOOL_NAME}" "${tool_path}/bin"
