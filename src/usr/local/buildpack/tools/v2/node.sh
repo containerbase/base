fe6b48f9cafe4b52176f655a849ba7d4d0ef1888
Chumper <Chumper@users.noreply.github.com>
2022-06-12 15:09:55 +0200

    feat: add node as v2 tool

---
+++ src/usr/local/buildpack/tools/v2/node.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+# Helper function to create a temp cache dir for node downloads
+function npm_init () {
+  temp_folder=$(mktemp -u)
+  mkdir -p "${temp_folder}"
+  export NPM_CONFIG_CACHE="${temp_folder}" NO_UPDATE_NOTIFIER=1 NPM_CONFIG_FUND=false
+}
+
+# Helper function to clean up the node cache dir
+function npm_clean () {
+  # Clean npm stuff
+  rm -rf "$HOME/.cache" "${NPM_CONFIG_CACHE}" "$HOME/.npm/_logs"/*
+}
+
+# Helper function to prepare a dir for node binaries
+function prepare_prefix () {
+  local prefix=${1}
+  # npm 7 bug
+  mkdir -p "${prefix}"/{bin,lib}
+}
+
+# Helper function to link to a globally installed node
+function prepare_global_config () {
+  local prefix=${1}
+  prepare_prefix "${prefix}"
+  mkdir -p "${versioned_tool_path}/etc"
+  echo "prefix = \"${prefix}\"" >> "${versioned_tool_path}/etc/npmrc"
+}
+
+# Helper function to link to a user installed node
+function prepare_user_config () {
+  local prefix=${1}
+  if grep 'prefix' "${USER_HOME}/.npmrc"; then
+    return
+  fi
+
+  prepare_prefix "${prefix}"
+  echo "prefix = \"${prefix}\"" >> "${USER_HOME}/.npmrc"
+  mkdir -p "${USER_HOME}/.npm/_logs"
+  chown -R "${USER_ID}" "${prefix}" "${USER_HOME}/.npmrc" "${USER_HOME}/.npm"
+  chmod -R g+w "${prefix}" "${USER_HOME}/.npmrc" "${USER_HOME}/.npm"
+}
+
+function check_tool_requirements () {
+  check_semver "$TOOL_VERSION"
+
+  if [[ ! "${MAJOR}" || ! "${MINOR}" ]]; then
+    echo Invalid version: "${TOOL_VERSION}"
+    exit 1
+  fi
+}
+
+function install_tool () {
+  local versioned_tool_path
+  local npm # temp npm executable
+
+  versioned_tool_path=$(create_versioned_tool_path)
+
+  # init temp dir
+  npm_init
+
+  npm=${versioned_tool_path}/bin/npm
+
+  # download file
+  file=$(get_from_url "https://nodejs.org/dist/v${TOOL_VERSION}/node-v${TOOL_VERSION}-linux-x64.tar.xz")
+  # extract
+  tar -C "${versioned_tool_path}" --strip 1 -xf "${file}"
+
+  if [[ $(is_root) -eq 0 ]]; then
+    # redirect root install
+    prepare_global_config /usr/local
+
+    # redirect user install
+    prepare_user_config "${USER_HOME}/.npm-global"
+  else
+    # redirect user install
+    prepare_global_config "${USER_HOME}/.npm-global"
+  fi
+
+  # required for npm
+  link_wrapper "${TOOL_NAME}" "${versioned_tool_path}/bin"
+
+  if [[ ${MAJOR} -lt 15 ]]; then
+    # update to latest node-gyp to fully support python3
+    $npm explore npm --global --prefix "$versioned_tool_path" -- "$npm" install node-gyp@latest --no-audit --cache "${NPM_CONFIG_CACHE}" 2>&1
+  fi
+
+  # clean temp dir
+  npm_clean
+}
+
+function link_tool () {
+  local versioned_tool_path
+  versioned_tool_path=$(find_versioned_tool_path)
+
+  local tool_env
+
+  reset_tool_env
+
+  link_wrapper "${TOOL_NAME}" "${versioned_tool_path}/bin"
+  link_wrapper npm "${versioned_tool_path}/bin"
+  link_wrapper npx "${versioned_tool_path}/bin"
+
+  export_tool_path "${USER_HOME}/.npm-global/bin"
+  export_tool_env NO_UPDATE_NOTIFIER 1
+  export_tool_env NPM_CONFIG_UPDATE_NOTIFIER false
+  export_tool_env NPM_CONFIG_FUND false
+
+  tool_env=$(find_tool_env)
+
+  cat >> "$tool_env" <<- EOM
+# openshift override unknown user home
+if [ "\${EUID}" != 0 ] && [ "\${EUID}" != ${USER_ID} ]; then
+  export NPM_CONFIG_PREFIX="${USER_HOME}/.npm-global"
+fi
+EOM
+
+  echo "node: $(node --version) $(command -v node)"
+  echo "npm: $(npm --version)  $(command -v npm)"
+}
