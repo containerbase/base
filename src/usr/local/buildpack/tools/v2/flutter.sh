35f4ebb16cf091e1de3984133975b36cd5664d01
Michael Kriese <michael.kriese@visualon.de>
2022-06-14 00:33:00 +0200

    feat: convert to v2 tools, support v2 and v3

---
+++ src/usr/local/buildpack/tools/v2/flutter.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+function prepare_tool() {
+  echo '{ "firstRun": false, "enabled": false }' > ~/.flutter
+  echo '{ "firstRun": false, "enabled": false }' > "${USER_HOME}/.flutter"
+  chown "${USER_NAME}" "${USER_HOME}/.flutter"
+  chmod -R g=u "${USER_HOME}/.flutter"
+}
+
+function install_tool () {
+  local versioned_tool_path
+  versioned_tool_path=$(create_versioned_tool_path)
+
+  local FLUTTER_SDK_CHANNEL="stable"
+  local FLUTTER_SDK_URL="https://storage.googleapis.com/flutter_infra_release/releases/${FLUTTER_SDK_CHANNEL}/linux/flutter_linux_${TOOL_VERSION}-${FLUTTER_SDK_CHANNEL}.tar.xz"
+  local file
+
+  if [[ ${MAJOR} -lt 2 || (${MAJOR} -eq 1 && ${MINOR} -lt 17) ]]; then
+    echo "flutter < 1.17.0 not supported" >&2
+    exit 1
+  fi
+
+  file=$(get_from_url "$FLUTTER_SDK_URL")
+  tar -C "${versioned_tool_path}" --strip 1 -xf "${file}"
+
+  # we need write access to some sub dirs for non root :-(
+  if [[ $(is_root) -eq 0 ]]; then
+    chgrp -R root "${versioned_tool_path}"
+    chmod -R g=u "${versioned_tool_path}"
+  fi
+
+  # git unsafe directory
+  git config --global --add safe.directory "${versioned_tool_path}"
+}
+
+function link_tool () {
+  local versioned_tool_path
+  versioned_tool_path=$(find_versioned_tool_path)
+
+  export_tool_env FLUTTER_ROOT "${versioned_tool_path}"
+  shell_wrapper "${TOOL_NAME}" "${versioned_tool_path}/bin"
+
+  flutter --version
+
+  if [[ $(is_root) -eq 0 ]]; then
+    su -c 'flutter --version' "${USER_NAME}"
+  fi
+}
