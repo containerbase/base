5a4dd40b564d9dcd0d73f98bce7a6cd523692d59
Chumper <Chumper@users.noreply.github.com>
2022-06-17 19:35:06 +0200

    feat(corepack): convert to v2 tool

---
+++ src/usr/local/buildpack/tools/v2/corepack.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function check_tool_requirements () {
+  check_command node
+  check_semver "$TOOL_VERSION"
+
+  if [[ ! "${MAJOR}" || ! "${MINOR}" || ! "${PATCH}" ]]; then
+    echo Invalid version: "${TOOL_VERSION}"
+    exit 1
+  fi
+}
+
+function install_tool () {
+  # get path location
+  DIR="${BASH_SOURCE%/*}"
+  if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+  # shellcheck source=/dev/null
+  . "$DIR/../../utils/node.sh"
+
+  npm_init
+  npm_install
+  npm_clean
+}
+
+function link_tool () {
+  local versioned_tool_path
+  versioned_tool_path=$(find_versioned_tool_path)
+
+  link_wrapper "${TOOL_NAME}" "${versioned_tool_path}/bin"
+  corepack --version
+  corepack enable --install-directory "$(get_bin_path)"
+}
