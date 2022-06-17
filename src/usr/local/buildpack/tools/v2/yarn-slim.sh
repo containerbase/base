e530778e1fb0dc63defb1c363f4664c6839d75a5
Nils Plaschke <Chumper@users.noreply.github.com>
2022-06-17 20:20:34 +0200

    convert yarn-slim to a v2 tool (#419)
    * feat(yarn-slim): convert to v2 tool
    
    * chore: adjust requirements check to new signature
    
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

---
+++ src/usr/local/buildpack/tools/v2/yarn-slim.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function check_tool_requirements () {
+  check_command node
+  check_semver "$TOOL_VERSION" "all"
+}
+
+function install_tool () {
+  local versioned_tool_path
+  versioned_tool_path=$(create_versioned_tool_path)
+
+  # get path location
+  DIR="${BASH_SOURCE%/*}"
+  if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+  # shellcheck source=/dev/null
+  . "$DIR/../../utils/node.sh"
+
+  npm_init
+  npm install "yarn@${TOOL_VERSION}" --global --no-audit --prefix "${versioned_tool_path}" --cache "${NPM_CONFIG_CACHE}" 2>&1
+  npm_clean
+
+  # patch yarn
+  sed -i 's/ steps,/ steps.slice(0,1),/' "${versioned_tool_path}/lib/node_modules/yarn/lib/cli.js"
+}
+
+function link_tool () {
+  local versioned_tool_path
+  versioned_tool_path=$(find_versioned_tool_path)
+
+  link_wrapper yarn "${versioned_tool_path}/bin/yarn"
+  yarn --version
+}
