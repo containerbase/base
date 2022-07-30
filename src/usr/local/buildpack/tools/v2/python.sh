8c1e93b7cee88959d1b5461b364faa85315d3c18
Jan Rotter <12612258+janrotter@users.noreply.github.com>
2022-07-31 00:22:40 +0200

    fix(python): fix linking an already installed version
    Before the fix, the following fails:
    > install-tool python 3.7.0 && install-tool python 3.6.0 && install-tool python 3.7.0
    with:
    > ...
    > linking tool python v3.7.0
    > No /opt/buildpack/tools/python/3.7.0/bin/python. defined - aborting
    
    The link_tool() function for python uses the MAJOR and MINOR env
    variables that are set by the check_semver(), called by
    the check_tool_requirements().
    
    The check_tool_requirements() is called only for a fresh install of the
    tool, so when linking an already installed version (e.g. to change the
    default python version), the check_tool_requirements() call is skipped,
    the MAJOR and MINOR env variables are missing and the error is raised.
    
    This change addresses this issue by calling the check_semver() in the
    link_tool() for python.

--- src/usr/local/buildpack/tools/v2/python.sh
+++ src/usr/local/buildpack/tools/v2/python.sh
@@ -95,6 +95,8 @@ function install_tool () {
 function link_tool () {
   local versioned_tool_path
   versioned_tool_path=$(find_versioned_tool_path)
+  
+  check_semver "${TOOL_VERSION}"
 
   reset_tool_env
 
