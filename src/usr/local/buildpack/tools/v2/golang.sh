6a661702fee9d94be3174deb05861d575320680f
Michael Kriese <michael.kriese@visualon.de>
2022-08-10 17:15:26 +0200

    feat(golang): convert to v2 tool

---
+++ src/usr/local/buildpack/tools/v2/golang.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+
+function prepare_tool() {
+
+  # go suggests: git svn bzr mercurial
+  apt_install bzr mercurial
+
+  export_env GOPATH "/go"
+  export_env CGO_ENABLED 0
+  export_env GOSUMDB off
+  export_path "\$GOPATH/bin"
+
+  mkdir -p "$GOPATH/src" "$GOPATH/bin" "$GOPATH/pkg"
+
+  chown -R "${USER_ID}" "$GOPATH"
+  chmod -R g+w "$GOPATH"
+  create_tool_path
+}
+
+function install_tool () {
+  local versioned_tool_path
+  local file
+  local ARCH
+  local GOLANG_FILE_VERSION
+
+  if [[ ! -d "$(find_tool_path)" ]]; then
+    if [[ $(is_root) -ne 0 ]]; then
+      echo "${TOOL_NAME} not prepared"
+      exit 1
+    fi
+    prepare_tool
+  fi
+
+  versioned_tool_path=$(create_versioned_tool_path)
+  ARCH=linux-amd64
+  # fix version
+  GOLANG_FILE_VERSION=${TOOL_VERSION}
+  if [[ "${PATCH}" == "0" ]]; then
+    GOLANG_FILE_VERSION="${MAJOR}.${MINOR}"
+  fi
+
+  file=$(get_from_url "https://dl.google.com/go/go${GOLANG_FILE_VERSION}.${ARCH}.tar.gz")
+  tar --strip 1 -C "${versioned_tool_path}" -xf "${file}"
+}
+
+function link_tool () {
+  local versioned_tool_path
+  versioned_tool_path=$(find_versioned_tool_path)
+
+  shell_wrapper go "${versioned_tool_path}/bin"
+
+  go version
+  go env
+}
