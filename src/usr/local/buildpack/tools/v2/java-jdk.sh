1ae22fa51801d184ff577016f8e15bbdcde26da0
Michael Kriese <michael.kriese@visualon.de>
2022-08-18 10:26:35 +0200

    feat(java): use central cacerts store

--- src/usr/local/buildpack/tools/v2/java-jdk.sh
+++ src/usr/local/buildpack/tools/v2/java-jdk.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+function prepare_tool () {
+  # shellcheck source=/dev/null
+  . "${BUILDPACK_DIR}/utils/java.sh"
+
+  prepare_java
+}
+
 function install_tool () {
   # shellcheck source=/dev/null
   . "${BUILDPACK_DIR}/utils/java.sh"
