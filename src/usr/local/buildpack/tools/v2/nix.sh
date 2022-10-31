d7f68357d529350cb7aacc29df38c8cc800cc877
Jamie Magee <jamie.magee@gmail.com>
2022-10-30 18:01:43 -0700

    feat(nix): convert to v2

---
+++ src/usr/local/buildpack/tools/v2/nix.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+function check_tool_requirements() {
+  check_semver "$TOOL_VERSION" "minor"
+  TOOL_VERSION=${MAJOR}.${MINOR}
+}
+
+function install_tool() {
+  local versioned_tool_path
+  local file
+  local ARCH
+
+  versioned_tool_path=$(create_versioned_tool_path)
+  ARCH=$(uname -m)
+
+  file=$(get_from_url "https://hydra.nixos.org/job/nix/maintenance-${TOOL_VERSION}/buildStatic.${ARCH}-linux/latest/download-by-type/file/binary-dist")
+
+  create_folder "${versioned_tool_path}/bin"
+  cp "${file}" "${versioned_tool_path}/bin/nix"
+  chmod +x "${versioned_tool_path}/bin/nix"
+}
+
+function link_tool() {
+  local versioned_tool_path
+  versioned_tool_path=$(find_versioned_tool_path)
+
+  shell_wrapper "${TOOL_NAME}" "${versioned_tool_path}/bin"
+  nix --version
+}
