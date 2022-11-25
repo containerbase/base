ece7d06caaecd1df2199df4a85c2091ca365d1ba
Michael Kriese <michael.kriese@visualon.de>
2022-11-25 16:21:10 +0100

    feat(rust): migrate to v2 tools

---
+++ src/usr/local/buildpack/tools/v2/rust.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function prepare_tool() {
+  export_env RUST_BACKTRACE 1
+  export_env CARGO_HOME "${USER_HOME}/.cargo"
+  export_path "\$CARGO_HOME/bin"
+}
+
+function install_tool () {
+  local versioned_tool_path
+  local file
+  local arch=x86_64
+  local tool_path
+  tool_path=$(find_tool_path)
+
+  if [[ ! -d "${tool_path}" ]]; then
+    if [[ $(is_root) -ne 0 ]]; then
+      echo "${TOOL_NAME} not prepared"
+      exit 1
+    fi
+    prepare_tool
+    tool_path=$(find_tool_path)
+  fi
+
+  versioned_tool_path=$(create_versioned_tool_path)
+
+  file=$(get_from_url "https://static.rust-lang.org/dist/rust-${TOOL_VERSION}-${arch}-unknown-linux-gnu.tar.gz")
+  mkdir -p "${TEMP_DIR}/rust"
+  bsdtar -C "${TEMP_DIR}/rust" -strip 1 -xf "${file}"
+  "${TEMP_DIR}/rust/install.sh" --prefix="$versioned_tool_path" --components=cargo,rust-std-${arch}-unknown-linux-gnu,rustc
+  rm -rf "${TEMP_DIR}/rust"
+}
+
+function link_tool () {
+  local versioned_tool_path
+  versioned_tool_path=$(find_versioned_tool_path)
+
+  shell_wrapper "cargo" "${versioned_tool_path}/bin"
+  shell_wrapper "rust" "${versioned_tool_path}/bin"
+
+  cargo --version
+  rustc --version
+}
