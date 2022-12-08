365c1588d6600b89af404b708cb68016b66f628c
Michael Kriese <michael.kriese@visualon.de>
2022-12-08 15:44:28 +0100

    fix(git-lfs): support install v3.1.0+

--- src/usr/local/buildpack/tools/git-lfs.sh
+++ src/usr/local/buildpack/tools/git-lfs.sh
@@ -17,10 +17,19 @@ fi
 
 ARCH=linux-amd64
 LFS_FILE="git-lfs-${ARCH}-v${TOOL_VERSION}.tar.gz"
+strip=0
+
+# v3.2+ has a subdir https://github.com/git-lfs/git-lfs/pull/4980
+if [[ ${MAJOR} -gt 3 || (${MAJOR} -eq 3 && ${MINOR} -ge 2) ]]; then
+  strip=1
+fi
 
 curl -sSfLo git-lfs.tgz "https://github.com/git-lfs/git-lfs/releases/download/v${TOOL_VERSION}/${LFS_FILE}"
-tar xzvf git-lfs.tgz -C /usr/local/bin git-lfs
+mkdir -p /tmp/git-lfs
+bsdtar --strip $strip -C /tmp/git-lfs -xf git-lfs.tgz
+mv /tmp/git-lfs/git-lfs /usr/local/bin/
 rm git-lfs.tgz
+rm -rf /tmp/git-lfs
 
 git lfs version
 git lfs install
