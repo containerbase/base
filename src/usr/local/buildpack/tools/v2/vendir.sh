63752301778a31ece81cf65f7c9652a5b8b5896a
Dominik Horb <git@dominik.berlin>
2022-12-26 21:26:27 +0100

    feat: add new tool vendir

---
+++ src/usr/local/buildpack/tools/v2/vendir.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function install_tool () {
+  local versioned_tool_path
+  versioned_tool_path=$(create_versioned_tool_path)
+
+  create_folder "${versioned_tool_path}/bin"
+
+  local file
+  file=$(get_from_url "https://github.com/vmware-tanzu/carvel-vendir/releases/download/v${TOOL_VERSION}/vendir-linux-amd64")
+  create_folder "${versioned_tool_path}/bin"
+  cp "${file}" "${versioned_tool_path}/bin/vendir"
+  chmod +x "${versioned_tool_path}/bin/vendir"
+}
+
+function link_tool () {
+  local versioned_tool_path
+  versioned_tool_path=$(find_versioned_tool_path)
+
+  shell_wrapper "${TOOL_NAME}" "${versioned_tool_path}/bin"
+  vendir --version
+}
