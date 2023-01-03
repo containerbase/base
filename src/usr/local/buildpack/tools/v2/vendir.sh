1ee5863a93a39e2e38155b630d42bdbdc4a81085
Michael Kriese <michael.kriese@visualon.de>
2023-01-03 09:18:31 +0100

    Apply suggestions from code review

--- src/usr/local/buildpack/tools/v2/vendir.sh
+++ src/usr/local/buildpack/tools/v2/vendir.sh
@@ -2,11 +2,11 @@
 
 function install_tool () {
   local versioned_tool_path
+  local file
   versioned_tool_path=$(create_versioned_tool_path)
 
   create_folder "${versioned_tool_path}/bin"
 
-  local file
   file=$(get_from_url "https://github.com/vmware-tanzu/carvel-vendir/releases/download/v${TOOL_VERSION}/vendir-linux-amd64")
   cp "${file}" "${versioned_tool_path}/bin/vendir"
   chmod +x "${versioned_tool_path}/bin/vendir"
