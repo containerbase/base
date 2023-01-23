4c9c0ca0a188471113aaaaee98009aa24b7dfbe1
Nikolai Røed Kristiansen <nikolaik@gmail.com>
2023-01-23 18:09:41 +0100

    feat: Add helmfile tool (#674)

---
+++ src/usr/local/buildpack/tools/v2/helmfile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function install_tool () {
+  local versioned_tool_path
+  local file
+  local arch=linux_amd64
+  local helmfile_file="${TOOL_NAME}_${TOOL_VERSION}_${arch}.tar.gz"
+
+  versioned_tool_path=$(create_versioned_tool_path)
+
+  create_folder "${versioned_tool_path}/bin"
+  file=$(get_from_url "https://github.com/${TOOL_NAME}/${TOOL_NAME}/releases/download/v${TOOL_VERSION}/${helmfile_file}")
+  bsdtar -C "${versioned_tool_path}/bin" -xf "${file}" "${TOOL_NAME}"
+}
+
+function link_tool () {
+  shell_wrapper "${TOOL_NAME}" "$(find_versioned_tool_path)/bin"
+  helmfile version
+}
