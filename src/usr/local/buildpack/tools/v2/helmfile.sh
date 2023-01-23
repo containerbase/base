be04696db3663504d4a4b3c1d4e0a957a815bf9c
Nikolai R Kristiansen <nikolai.kristiansen@remarkable.no>
2023-01-23 11:56:53 +0100

    chore: address review feedback

--- src/usr/local/buildpack/tools/v2/helmfile.sh
+++ src/usr/local/buildpack/tools/v2/helmfile.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-function check_tool_requirements () {
-  check_semver "$TOOL_VERSION" "all"
-}
-
 function install_tool () {
   local versioned_tool_path
   local file
   local arch=linux_amd64
-  local helmfile_file="${TOOL_NAME}_${TOOL_VERSION#v}_${arch}.tar.gz"
+  local helmfile_file="${TOOL_NAME}_${TOOL_VERSION}_${arch}.tar.gz"
 
   versioned_tool_path=$(create_versioned_tool_path)
 
@@ -18,8 +14,6 @@ function install_tool () {
 }
 
 function link_tool () {
-  local versioned_tool_path
-
   shell_wrapper "${TOOL_NAME}" "$(find_versioned_tool_path)/bin"
   helmfile version
 }
