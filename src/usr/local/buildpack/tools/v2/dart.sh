550d3fd51608ad6ab87c459d7b9095d3a8f69f5f
Michael Kriese <michael.kriese@visualon.de>
2023-03-17 08:58:22 +0100

    feat(dart): support arm64 and checksums

--- src/usr/local/buildpack/tools/v2/dart.sh
+++ src/usr/local/buildpack/tools/v2/dart.sh
@@ -10,11 +10,22 @@ function prepare_tool() {
 
 # https://storage.googleapis.com/dart-archive/channels/stable/release/1.11.0/sdk/dartsdk-linux-x64-release.zip
 # https://storage.googleapis.com/dart-archive/channels/stable/release/2.18.0/sdk/dartsdk-linux-x64-release.zip
+# https://storage.googleapis.com/dart-archive/channels/stable/release/2.19.4/sdk/dartsdk-linux-x64-release.zip.sha256sum
+# https://storage.googleapis.com/dart-archive/channels/stable/release/2.19.4/sdk/dartsdk-linux-arm64-release.zip
+# https://storage.googleapis.com/dart-archive/channels/stable/release/2.19.4/sdk/dartsdk-linux-arm64-release.zip.sha256sum
 function install_tool () {
   local versioned_tool_path
   local DART_SDK_CHANNEL="stable"
-  local DART_SDK_URL="https://storage.googleapis.com/dart-archive/channels/${DART_SDK_CHANNEL}/release/${TOOL_VERSION}/sdk/dartsdk-linux-x64-release.zip"
+  local DART_SDK_URL="https://storage.googleapis.com/dart-archive/channels/${DART_SDK_CHANNEL}/release/${TOOL_VERSION}/sdk"
   local file
+  local arch=x64
+
+  local checksum_file
+  local expected_checksum
+
+  if [[ "${ARCHITECTURE}" = "aarch64" ]]; then
+    arch=arm64
+  fi
 
   # do we need those old unsupported dart versions?
   # if [[ $MAJOR -lt 1 || ($MAJOR -eq 1 && $MINOR -lt 11) ]]; then
@@ -27,7 +38,11 @@ function install_tool () {
     exit 1
   fi
 
-  file=$(get_from_url "$DART_SDK_URL")
+  checksum_file=$(get_from_url "${DART_SDK_URL}/dartsdk-linux-${arch}-release.zip.sha256sum")
+  # get checksum from file
+  expected_checksum=$(grep "\\*dartsdk-linux-${arch}-release.zip" "${checksum_file}" | cut -d' ' -f1)
+
+  file=$(get_from_url "${DART_SDK_URL}/dartsdk-linux-${arch}-release.zip" "${expected_checksum}" "sha256sum")
 
   versioned_tool_path=$(create_versioned_tool_path)
   bsdtar -C "${versioned_tool_path}" --strip 1 -xf "${file}"
