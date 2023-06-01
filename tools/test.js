0bbc5315de6c26ab68207d614a1ac7b071fd8660
Michael Kriese <michael.kriese@visualon.de>
2023-06-01 09:46:52 +0200

    test: extend docker test command

--- tools/test.js
+++ tools/test.js
@@ -1,12 +1,39 @@
 import { promises as fs } from 'fs';
+import { Command, Option, runExit } from 'clipanion';
 import shell from 'shelljs';
 
 shell.config.fatal = true;
 
-for (const d of shell.ls('test')) {
-  if (!(await fs.stat(`test/${d}/Dockerfile`).catch(() => null))?.isFile()) {
-    continue;
+class TestCommand extends Command {
+  tests = Option.Rest({ required: false });
+  dryRun = Option.Boolean('-d, --dry-run', { required: false });
+
+  async execute() {
+    let tests = this.tests;
+    let explicit = true;
+
+    if (!tests.length) {
+      tests = shell.ls('test');
+      explicit = false;
+      shell.echo('Running all tests');
+    }
+
+    for (const d of tests) {
+      if (
+        !(await fs.stat(`test/${d}/Dockerfile`).catch(() => null))?.isFile()
+      ) {
+        if (explicit) {
+          shell.echo(`test '${d}' not found!`);
+          return 1;
+        }
+        continue;
+      }
+      shell.echo('Processing:', d);
+      shell.exec('docker buildx bake test', {
+        env: { ...process.env, TAG: d },
+      });
+    }
   }
-  shell.echo('Processing:', d);
-  shell.exec('docker buildx bake test', { env: { ...process.env, TAG: d } });
 }
+
+void runExit(TestCommand);
