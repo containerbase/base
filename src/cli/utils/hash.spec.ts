a77fd6ffe0487159fe4650dfcc02ef9ebe33d404
Michael Kriese <michael.kriese@visualon.de>
2023-07-13 22:28:52 +0200

    fix(http): compute checksum after download (#1196)

---
+++ src/cli/utils/hash.spec.ts
@@ -0,0 +1,20 @@
+import fs from 'node:fs/promises';
+import { env } from 'node:process';
+import { describe, expect, test } from 'vitest';
+import { hash, hashFile } from './hash';
+
+describe('hash', () => {
+  test('should hash data with sha256', () => {
+    expect(hash('https://example.com/test.txt', 'sha256')).toBe(
+      'd1dc63218c42abba594fff6450457dc8c4bfdd7c22acf835a50ca0e5d2693020'
+    );
+  });
+
+  test('should hash file with sha256', async () => {
+    const file = `${env.CONTAINERBASE_CACHE_DIR}/test.txt`;
+    await fs.writeFile(file, 'https://example.com/test.txt');
+    expect(await hashFile(file, 'sha256')).toBe(
+      'd1dc63218c42abba594fff6450457dc8c4bfdd7c22acf835a50ca0e5d2693020'
+    );
+  });
+});
---
+++ src/cli/utils/hash.ts
@@ -0,0 +1,24 @@
+import crypto from 'node:crypto';
+import fs from 'node:fs/promises';
+import type { LiteralUnion } from 'type-fest';
+
+export type AlgorithmName = LiteralUnion<
+  'sha1' | 'sha224' | 'sha256' | 'sha384' | 'sha512',
+  string
+>;
+
+export function hash(data: string | Buffer, algorithm: AlgorithmName): string {
+  const hash = crypto.createHash(algorithm);
+  hash.update(data);
+  return hash.digest('hex');
+}
+
+export async function hashFile(
+  file: string,
+  algorithm: AlgorithmName
+): Promise<string> {
+  const data = await fs.readFile(file);
+  const hash = crypto.createHash(algorithm);
+  hash.update(data);
+  return hash.digest('hex');
+}
