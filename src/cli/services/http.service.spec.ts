5d06af9260160ebb0dab8c25e1a71293fa88a7e8
Michael Kriese <michael.kriese@visualon.de>
2023-07-13 13:34:32 +0200

    feat: support url replace via env

--- src/cli/services/http.service.spec.ts
+++ src/cli/services/http.service.spec.ts
@@ -9,19 +9,24 @@ describe('http.service', () => {
 
   beforeEach(() => {
     child = rootContainer.createChild();
+
+    for (const key of Object.keys(env)) {
+      if (key.startsWith('URL_REPLACE_')) {
+        delete env[key];
+      }
+    }
   });
 
   test('throws', async () => {
-    scope('https://example.com').get('/fail.txt').times(3).reply(404);
+    scope('https://example.com').get('/fail.txt').times(6).reply(404);
     const http = child.get(HttpService);
 
     await expect(
       http.download({ url: 'https://example.com/fail.txt' })
     ).rejects.toThrow();
-    // bug, currently resolves
-    // await expect(
-    //   http.download({ url: 'https://example.com/fail.txt' })
-    // ).rejects.toThrow();
+    await expect(
+      http.download({ url: 'https://example.com/fail.txt' })
+    ).rejects.toThrow();
   });
 
   test('download', async () => {
@@ -33,8 +38,25 @@ describe('http.service', () => {
       `${env.CONTAINERBASE_CACHE_DIR}/d1dc63218c42abba594fff6450457dc8c4bfdd7c22acf835a50ca0e5d2693020/test.txt`
     );
     // uses cache
-    // expect(await http.download({ url: 'https://example.com/test.txt' })).toBe(
-    //   `${env.CONTAINERBASE_CACHE_DIR}/d1dc63218c42abba594fff6450457dc8c4bfdd7c22acf835a50ca0e5d2693020/test.txt`
-    // );
+    expect(await http.download({ url: 'https://example.com/test.txt' })).toBe(
+      `${env.CONTAINERBASE_CACHE_DIR}/d1dc63218c42abba594fff6450457dc8c4bfdd7c22acf835a50ca0e5d2693020/test.txt`
+    );
+  });
+
+  test('replaces url', async () => {
+    scope('https://example.org').get('/test.txt').reply(200, 'ok');
+
+    env.URL_REPLACE_0_FROM = 'https://example.com';
+    env.URL_REPLACE_0_TO = 'https://example.org';
+
+    const http = child.get(HttpService);
+
+    expect(await http.download({ url: 'https://example.org/test.txt' })).toBe(
+      `${env.CONTAINERBASE_CACHE_DIR}/d1dc63218c42abba594fff6450457dc8c4bfdd7c22acf835a50ca0e5d2693020/test.txt`
+    );
+    // uses cache
+    expect(await http.download({ url: 'https://example.org/test.txt' })).toBe(
+      `${env.CONTAINERBASE_CACHE_DIR}/d1dc63218c42abba594fff6450457dc8c4bfdd7c22acf835a50ca0e5d2693020/test.txt`
+    );
   });
 });
