6b8670c2b542e9990056b84d361b21978e61a89e
Michael Kriese <michael.kriese@visualon.de>
2023-08-24 15:26:31 +0200

    feat(python): allow replacing pip index

--- src/cli/install-tool/install-legacy-tool.service.ts
+++ src/cli/install-tool/install-legacy-tool.service.ts
@@ -1,13 +1,26 @@
 import { execa } from 'execa';
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
+import { EnvService } from '../services';
 import { logger } from '../utils';
+
+const defaultPipRegistry = 'https://pypi.org/simple/';
 
 @injectable()
 export class InstallLegacyToolService {
+  constructor(@inject(EnvService) private readonly envSvc: EnvService) {}
+
   async execute(tool: string, version: string): Promise<void> {
     logger.debug(`Installing legacy tool ${tool} v${version} ...`);
+    const env: NodeJS.ProcessEnv = {};
+
+    const pipIndex = this.envSvc.replaceUrl(defaultPipRegistry);
+    if (pipIndex !== defaultPipRegistry) {
+      env.PIP_INDEX_URL = pipIndex;
+    }
+
     await execa('/usr/local/containerbase/bin/install-tool', [tool, version], {
       stdio: ['inherit', 'inherit', 1],
+      env,
     });
   }
 }
