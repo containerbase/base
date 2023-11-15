f45fdbb26c4b70e21321500387eb2fa217b6982a
Andreas Schleifer <andreas.schleifer@exb.de>
2023-11-15 15:03:54 +0100

    feat(tools): add bazelisk

---
+++ src/cli/tools/bazelisk/index.ts
@@ -0,0 +1,62 @@
+import fs from 'node:fs/promises';
+import { join } from 'node:path';
+import { execa } from 'execa';
+import { inject, injectable } from 'inversify';
+import { InstallToolBaseService } from '../../install-tool/install-tool-base.service';
+import { EnvService, HttpService, PathService } from '../../services';
+
+@injectable()
+export class InstallBazeliskService extends InstallToolBaseService {
+  readonly name = 'bazelisk';
+
+  private get arch(): string {
+    return this.envSvc.arch;
+  }
+
+  constructor(
+    @inject(EnvService) envSvc: EnvService,
+    @inject(PathService) pathSvc: PathService,
+    @inject(HttpService) private http: HttpService,
+  ) {
+    super(pathSvc, envSvc);
+  }
+
+  override async install(version: string): Promise<void> {
+    const baseurl = `https://github.com/bazelbuild/bazelisk/releases/download/v${version}/`;
+    const filename = `bazelisk-linux-${this.arch}`;
+
+    const file = await this.http.download({
+      url: `${baseurl}${filename}`,
+    });
+
+    // TODO: create recursive
+    if (!(await this.pathSvc.findToolPath(this.name))) {
+      await this.pathSvc.createToolPath(this.name);
+    }
+
+    const path = join(
+      await this.pathSvc.createVersionedToolPath(this.name, version),
+      'bin',
+    );
+    await fs.mkdir(path);
+
+    const binarypath = join(path, 'bazelisk');
+    await fs.copyFile(file, binarypath);
+    await this.pathSvc.setOwner({
+      file: binarypath,
+    });
+  }
+
+  override async link(version: string): Promise<void> {
+    const src = join(this.pathSvc.versionedToolPath(this.name, version), 'bin');
+
+    await this.shellwrapper({
+      srcDir: src,
+    });
+    await fs.symlink('bazelisk', join(this.pathSvc.binDir, 'bazel'));
+  }
+
+  override async test(_version: string): Promise<void> {
+    await execa('bazelisk', ['version'], { stdio: ['inherit', 'inherit', 1] });
+  }
+}
--- src/cli/tools/index.ts
+++ src/cli/tools/index.ts
@@ -1,4 +1,5 @@
 export const NoPrepareTools = [
+  'bazelisk',
   'bower',
   'bun',
   'bundler',
