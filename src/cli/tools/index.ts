393f0f54f301b40cc88331b925b9d69d3a594c26
Michael Kriese <michael.kriese@visualon.de>
2024-01-03 15:39:52 +0100

    fix(cli): remove unused maps

--- src/cli/tools/index.ts
+++ src/cli/tools/index.ts
@@ -18,16 +18,22 @@ export const NoPrepareTools = [
   'yarn-slim',
 ];
 
+/**
+ * Tools in this map are implicit mapped from `install-tool` to `install-<type>`.
+ * So no need for an extra install service.
+ */
 export const ResolverMap: Record<string, InstallToolType | undefined> = {
   bundler: 'gem',
   corepack: 'npm',
   npm: 'npm',
   pnpm: 'npm',
-  renovate: 'npm',
   yarn: 'npm',
-  'yarn-slim': 'npm',
 };
 
+/**
+ * This tools are deprecated and should not be used anymore via `install-tool`.
+ * They are implicit mapped from `install-tool` to `install-<type>`.
+ */
 export const DeprecatedTools: Record<string, InstallToolType | undefined> = {
   bower: 'npm',
   lerna: 'npm',
