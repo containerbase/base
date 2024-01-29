b86b6967d8e4f9be526862008758fc578b91bcdc
Michael Kriese <michael.kriese@visualon.de>
2024-01-29 11:12:57 +0100

    test: fix mocking

--- test/global-setup.ts
+++ test/global-setup.ts
@@ -1,19 +1,27 @@
 import 'reflect-metadata';
-import fs from 'node:fs/promises';
-import { env } from 'node:process';
 import { afterAll, beforeAll, vi } from 'vitest';
 
 vi.mock('pino');
 
 beforeAll(async () => {
+  const fs =
+    await vi.importActual<typeof import('node:fs/promises')>(
+      'node:fs/promises',
+    );
   globalThis.cacheDir = await fs.mkdtemp('/tmp/containerbase-test-');
   globalThis.rootDir = await fs.mkdtemp('/tmp/containerbase-root-');
+
+  const { env } = await import('node:process');
 
   env.CONTAINERBASE_CACHE_DIR = globalThis.cacheDir;
   env.CONTAINERBASE_ROOT_DIR = globalThis.rootDir;
 });
 
 afterAll(async () => {
+  const fs =
+    await vi.importActual<typeof import('node:fs/promises')>(
+      'node:fs/promises',
+    );
   await fs.rm(globalThis.cacheDir, { recursive: true });
   await fs.rm(globalThis.rootDir, { recursive: true });
 });
