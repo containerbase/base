7416d49ec836b77c53b3934bec8637afabd8b1b9
Michael Kriese <michael.kriese@visualon.de>
2024-06-06 14:21:17 +0200

    fix(cli): drop node-tar and use native bsdtar

--- src/cli/services/compression.service.ts
+++ src/cli/services/compression.service.ts
@@ -1,6 +1,5 @@
 import { execa } from 'execa';
 import { injectable } from 'inversify';
-import { extract } from 'tar/extract';
 
 export interface ExtractConfig {
   file: string;
@@ -13,25 +12,13 @@ export interface ExtractConfig {
 @injectable()
 export class CompressionService {
   async extract({ file, cwd, strip, files }: ExtractConfig): Promise<void> {
-    if (
-      file.endsWith('.zip') ||
-      file.endsWith('.tar.xz') ||
-      file.endsWith('.txz')
-    ) {
-      await execa('bsdtar', [
-        '-xf',
-        file,
-        '-C',
-        cwd,
-        ...(strip ? ['--strip', `${strip}`] : []),
-        ...(files ?? []),
-      ]);
-      return;
-    }
-
-    await extract(
-      { cwd, ...(strip ? { strip } : {}), newer: true, keep: false, file },
-      files,
-    );
+    await execa('bsdtar', [
+      '-xf',
+      file,
+      '-C',
+      cwd,
+      ...(strip ? ['--strip', `${strip}`] : []),
+      ...(files ?? []),
+    ]);
   }
 }
