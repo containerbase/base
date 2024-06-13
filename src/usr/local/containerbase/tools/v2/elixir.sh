3d9d0895e4d4d2840247ea955de5e7b57d154861
Pierre Cavin <me@sherlox.io>
2024-06-13 11:43:31 +0200

    fix: add support for Elixir v1.17.0 (#2803)
    * feat: add support for Elixir v1.17.0
    
    * fix: fix for Elixir < 1.14
    
    * fix: cough cough
    
    * fix: use OTP 25 for 1.14 <= x <= 1.16
    
    * fix: add back compability code for older elixir versions
    
    * fix: maximize OTP compatibilite for 1.17

--- src/usr/local/containerbase/tools/v2/elixir.sh
+++ src/usr/local/containerbase/tools/v2/elixir.sh
@@ -8,12 +8,14 @@ function install_tool () {
 
   check_command erl
 
-
   # https://github.com/elixir-lang/elixir/releases/tag/v1.14.0
-  if [ "$MAJOR" -eq 1 ] && [ "$MINOR" -eq 14 ]; then
-    base_file=elixir-otp-23.zip
-  elif [ "$MAJOR" -eq 1 ] && [ "$MINOR" -gt 14 ]; then
+  # https://hexdocs.pm/elixir/compatibility-and-deprecations.html#between-elixir-and-erlang-otp
+  if [ "$MAJOR" -eq 1 ] && [ "$MINOR" -ge 17 ]; then
+    base_file=elixir-otp-25.zip
+  elif [ "$MAJOR" -eq 1 ] && [ "$MINOR" -ge 15 ]; then
     base_file=elixir-otp-24.zip
+  elif [ "$MAJOR" -eq 1 ] && [ "$MINOR" -eq 14 ]; then
+    base_file=elixir-otp-23.zip
   fi
 
   file=$(get_from_url "${base_url}/v${TOOL_VERSION}/${base_file}")
