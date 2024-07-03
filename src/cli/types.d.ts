01e7c3817cbec0452b56e1d0dc55149d585a9c2c
Michael Kriese <michael.kriese@visualon.de>
2024-07-04 00:19:14 +0200

    chore: fix types

--- src/cli/types.d.ts
+++ src/cli/types.d.ts
@@ -1,3 +1,6 @@
 interface Error {
   code?: string;
 }
+
+/* eslint-disable no-var */
+declare var rootDir: string | undefined;
--- test/types.d.ts
+++ test/types.d.ts
@@ -1,5 +1,2 @@
 /* eslint-disable no-var */
-declare module globalThis {
-  var cacheDir: string;
-  var rootDir: string | undefined;
-}
+declare var cacheDir: string;
