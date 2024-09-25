c4497dd4362d03e012e430464da6ea93749ee356
Michael Kriese <michael.kriese@visualon.de>
2024-09-25 13:07:39 +0200

    fix(php)!: drop `getcomposer.org` download support

--- src/cli/tools/php/composer.ts
+++ src/cli/tools/php/composer.ts
@@ -12,8 +12,7 @@ import {
   HttpService,
   PathService,
 } from '../../services';
-import type { HttpChecksumType } from '../../services/http.service';
-import { logger, semverSort } from '../../utils';
+import { semverSort } from '../../utils';
 
 @injectable()
 export class ComposerInstallService extends BaseInstallService {
@@ -30,63 +29,19 @@ export class ComposerInstallService extends BaseInstallService {
 
   override async install(version: string): Promise<void> {
     const name = this.name;
-    let filename = `${name}-${version}.tar.xz`;
-    let url = `https://github.com/containerbase/${name}-prebuild/releases/download/${version}/${filename}`;
-    let checksumFileUrl = `${url}.sha512`;
-    const isOnGithub = await this.http.exists(checksumFileUrl);
-    let file: string;
+    const filename = `${name}-${version}.tar.xz`;
+    const url = `https://github.com/containerbase/${name}-prebuild/releases/download/${version}/${filename}`;
 
-    if (isOnGithub) {
-      logger.info(`using github`);
-      const checksumFile = await this.http.download({ url: checksumFileUrl });
-      const expectedChecksum = (
-        await fs.readFile(checksumFile, 'utf-8')
-      ).trim();
-      file = await this.http.download({
-        url,
-        checksumType: 'sha512',
-        expectedChecksum,
-      });
-    } else {
-      logger.info(`using getcomposer.org`);
-      // fallback to getcomposer.org
-      filename = `composer.phar`;
-      url = `https://getcomposer.org/download/${version}/composer.phar`;
-      checksumFileUrl = `${url}.sha256sum`;
-      let expectedChecksum: string | undefined;
-      let checksumType: HttpChecksumType | undefined;
-      if (await this.http.exists(checksumFileUrl)) {
-        logger.debug(`using sha256sum checksum for ${filename}`);
-        expectedChecksum = await this.readChecksum(`${url}.sha256sum`);
-        checksumType = 'sha256';
-      } else {
-        throw new Error(`checksum file not found for ${filename}`);
-      }
+    const checksumFile = await this.http.download({ url: `${url}.sha512` });
+    const expectedChecksum = (await fs.readFile(checksumFile, 'utf-8')).trim();
+    const file = await this.http.download({
+      url,
+      checksumType: 'sha512',
+      expectedChecksum,
+    });
 
-      if (!checksumType || !expectedChecksum) {
-        throw new Error(`checksum not found for ${filename}`);
-      }
-
-      file = await this.http.download({
-        url,
-        checksumType,
-        expectedChecksum,
-      });
-    }
-
-    let path = await this.pathSvc.ensureToolPath(this.name);
-
-    if (isOnGithub) {
-      await this.compress.extract({ file, cwd: path });
-    } else {
-      // from getcomposer.org
-      path = await this.pathSvc.createVersionedToolPath(this.name, version);
-      path = join(path, 'bin');
-      await fs.mkdir(path);
-      path = join(path, 'composer');
-      await fs.cp(file, path);
-      await fs.chmod(path, this.envSvc.umask);
-    }
+    const path = await this.pathSvc.ensureToolPath(this.name);
+    await this.compress.extract({ file, cwd: path });
   }
 
   override async link(version: string): Promise<void> {
@@ -98,11 +53,6 @@ export class ComposerInstallService extends BaseInstallService {
     await execa('composer', ['--version'], {
       stdio: ['inherit', 'inherit', 1],
     });
-  }
-
-  private async readChecksum(url: string): Promise<string | undefined> {
-    const checksumFile = await this.http.download({ url });
-    return (await fs.readFile(checksumFile, 'utf-8')).split(' ')[0]?.trim();
   }
 }
 
