98cfed7d184944b1bb8cd1233b8b5d060b7f5989
Michael Kriese <michael.kriese@visualon.de>
2024-10-01 11:17:30 +0200

    feat(nix)!: drop install support from `hydra.nixos.org` (#3358)
    - Closes #3333

--- src/usr/local/containerbase/tools/v2/nix.sh
+++ src/usr/local/containerbase/tools/v2/nix.sh
@@ -1,54 +1,34 @@
 #!/usr/bin/env bash
 
 function install_tool() {
-  local versioned_tool_path
-  local file
   local arch
-  local build_path
+  local checksum_file
+  local expected_checksum
+  local file
   local name=${TOOL_NAME}
   local tool_path
   local version=${TOOL_VERSION}
+  local versioned_tool_path
 
   if [[ ${MAJOR} -lt 2 || (${MAJOR} -eq 2 && ${MINOR} -lt 10) || (${MAJOR} -eq 2 && ${MINOR} -eq 10 && ${PATCH} -lt 3) ]]; then
-    echo "Nix version ${TOOL_VERSION} is not supported! Use v2.10.3 or higher." >&2
+    echo "Nix version ${version} is not supported! Use v2.10.3 or higher." >&2
     exit 1
   fi
 
   arch=$(uname -m)
   base_url="https://github.com/containerbase/${name}-prebuild/releases/download"
 
-  # not all releases have checksums
-  checksum_exists=$(file_exists "${base_url}/${version}/${name}-${version}-${arch}.tar.xz.sha512")
-  if [[ "${checksum_exists}" == "200" ]]; then
-    echo "Downloading ${name} ${version} for ${arch} from github ..."
-    tool_path=$(create_tool_path)
-    checksum_file=$(get_from_url "${base_url}/${version}/${name}-${version}-${arch}.tar.xz.sha512")
-    # get checksum from file
-    expected_checksum=$(cat "${checksum_file}")
-    file=$(get_from_url \
-      "${base_url}/${version}/${name}-${version}-${arch}.tar.xz" \
-      "${name}-${version}-${arch}.tar.xz" \
-      "${expected_checksum}" \
-      sha512sum
-    )
+  tool_path=$(create_tool_path)
+  checksum_file=$(get_from_url "${base_url}/${version}/${name}-${version}-${arch}.tar.xz.sha512")
+  expected_checksum=$(cat "${checksum_file}")
+  file=$(get_from_url \
+    "${base_url}/${version}/${name}-${version}-${arch}.tar.xz" \
+    "${name}-${version}-${arch}.tar.xz" \
+    "${expected_checksum}" \
+    sha512sum
+  )
 
-    bsdtar -C "${tool_path}" -xf "${file}"
-  else
-    TOOL_VERSION="${MAJOR}.${MINOR}"
-    echo "Downloading ${name} ${version} for ${arch} from hydra ..."
-    if [[ ${MAJOR} -lt 2 || (${MAJOR} -eq 2 && ${MINOR} -lt 24) ]]; then
-      build_path="buildStatic.${arch}-linux"
-    else
-      build_path="buildStatic.nix.${arch}-linux"
-    fi
-
-    file=$(get_from_url "https://hydra.nixos.org/job/nix/maintenance-${TOOL_VERSION}/${build_path}/latest/download-by-type/file/binary-dist")
-
-    versioned_tool_path=$(create_versioned_tool_path)
-    create_folder "${versioned_tool_path}/bin"
-    cp "${file}" "${versioned_tool_path}/bin/nix"
-    chmod +x "${versioned_tool_path}/bin/nix"
-  fi
+  bsdtar -C "${tool_path}" -xf "${file}"
 }
 
 function link_tool() {
