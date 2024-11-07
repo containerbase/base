08963ab19a80000ef43f94d4965bbb791debeba0
Michael Kriese <michael.kriese@visualon.de>
2024-11-07 09:08:05 +0100

    fix: init prepared tools from entrypoint (#3568)
    * fix: init prepared tools from entrypoint
    
    * test: disable test
    
    * test: fix tests

--- src/cli/prepare-tool/index.spec.ts
+++ src/cli/prepare-tool/index.spec.ts
@@ -1,6 +1,7 @@
 import fs from 'node:fs/promises';
 import { beforeAll, describe, expect, test, vi } from 'vitest';
 import { rootPath } from '../../../test/path';
+import { PathService, rootContainer } from '../services';
 import { initializeTools, prepareTools } from '.';
 
 vi.mock('del');
@@ -30,6 +31,10 @@ describe('index', () => {
       rootPath('usr/local/containerbase/tools/v2/dummy.sh'),
       '',
     );
+
+    const child = rootContainer.createChild();
+    const pathSvc = child.get(PathService);
+    await pathSvc.setPrepared('bun');
   });
 
   test('prepareTools', async () => {
@@ -40,5 +45,6 @@ describe('index', () => {
   test('initializeTools', async () => {
     expect(await initializeTools(['bun', 'dummy'])).toBeUndefined();
     expect(await initializeTools(['not-exist'])).toBeUndefined();
+    expect(await initializeTools(['all'])).toBeUndefined();
   });
 });
