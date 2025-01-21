8ff624c21083c8bd6abc0eaaeb9311616714d6f9
Omer Cohen <git@omerc.net>
2025-01-21 11:15:26 +0200

    feat(tool): add protoc (#3947)
    * feat(tool): add protoc
    
    * fix
    
    * coerceSemver
    
    * override validate only in protoc
    
    * Update renovate.json
    
    Co-authored-by: Michael Kriese <michael.kriese@gmx.de>
    
    * Update src/cli/tools/protoc.ts
    
    Co-authored-by: Michael Kriese <michael.kriese@gmx.de>
    
    * lint
    
    ---------
    
    Co-authored-by: Michael Kriese <michael.kriese@gmx.de>

---
+++ src/cli/tools/protoc.ts
@@ -0,0 +1,66 @@
+import path from 'node:path';
+import { execa } from 'execa';
+import { inject, injectable } from 'inversify';
+import { BaseInstallService } from '../install-tool/base-install.service';
+import {
+  CompressionService,
+  EnvService,
+  HttpService,
+  PathService,
+} from '../services';
+import { semverCoerce } from '../utils';
+
+@injectable()
+export class ProtocInstallService extends BaseInstallService {
+  readonly name = 'protoc';
+
+  private get ghArch(): string {
+    switch (this.envSvc.arch) {
+      case 'arm64':
+        return 'aarch_64';
+      case 'amd64':
+        return 'x86_64';
+    }
+  }
+
+  constructor(
+    @inject(EnvService) envSvc: EnvService,
+    @inject(PathService) pathSvc: PathService,
+    @inject(HttpService) private http: HttpService,
+    @inject(CompressionService) private compress: CompressionService,
+  ) {
+    super(pathSvc, envSvc);
+  }
+
+  override async install(version: string): Promise<void> {
+    const name = this.name;
+
+    const url = `https://github.com/protocolbuffers/protobuf/releases/download/v${version}/${name}-${version}-linux-${this.ghArch}.zip`;
+
+    const file = await this.http.download({
+      url,
+    });
+
+    const cwd = await this.pathSvc.createVersionedToolPath(name, version);
+
+    await this.compress.extract({ file, cwd });
+  }
+
+  override async link(version: string): Promise<void> {
+    const src = path.join(
+      this.pathSvc.versionedToolPath(this.name, version),
+      'bin',
+    );
+    await this.shellwrapper({ srcDir: src });
+  }
+
+  override async test(_version: string): Promise<void> {
+    await execa(this.name, ['--version'], {
+      stdio: ['inherit', 'inherit', 1],
+    });
+  }
+
+  override validate(version: string): Promise<boolean> {
+    return Promise.resolve(semverCoerce(version) !== null);
+  }
+}
