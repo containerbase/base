da9d3a7474a61aada1ba900d3685a7437235d7db
Trim21 <trim21.me@gmail.com>
2025-02-25 02:19:18 +0800

    feat: add new tool pixi (#4161)
    * feat: add new tool pixi
    
    * typo
    
    * add tests
    
    * add tests
    
    * add bin directory to output
    
    * do not strip

---
+++ src/cli/tools/pixi.ts
@@ -0,0 +1,74 @@
+import fs from 'node:fs/promises';
+import { join } from 'node:path';
+import { execa } from 'execa';
+import { inject, injectable } from 'inversify';
+import { BaseInstallService } from '../install-tool/base-install.service';
+import {
+  CompressionService,
+  EnvService,
+  HttpService,
+  PathService,
+} from '../services';
+
+@injectable()
+export class PixiInstallService extends BaseInstallService {
+  readonly name = 'pixi';
+
+  private get ghArch(): string {
+    switch (this.envSvc.arch) {
+      case 'arm64':
+        return 'aarch64';
+      case 'amd64':
+        return 'x86_64';
+    }
+  }
+
+  constructor(
+    @inject(EnvService) envSvc: EnvService,
+    @inject(PathService) pathSvc: PathService,
+    @inject(HttpService) private http: HttpService,
+    @inject(CompressionService) private compress: CompressionService,
+  ) {
+    super(pathSvc, envSvc);
+  }
+
+  override async install(version: string): Promise<void> {
+    const url = `https://github.com/prefix-dev/pixi/releases/download/v${version}/${this.name}-${this.ghArch}-unknown-linux-musl.tar.gz`;
+    const checksumFileUrl = `${url}.sha256`;
+
+    const checksumFile = await this.http.download({ url: checksumFileUrl });
+    const expectedChecksum = (await fs.readFile(checksumFile, 'utf-8'))
+      .trim()
+      .split(' ')[0];
+
+    const file = await this.http.download({
+      url,
+      checksumType: 'sha256',
+      expectedChecksum,
+    });
+
+    await this.pathSvc.ensureToolPath(this.name);
+
+    const path = join(
+      await this.pathSvc.createVersionedToolPath(this.name, version),
+      'bin',
+    );
+    await fs.mkdir(path);
+    await this.compress.extract({
+      file,
+      cwd: path,
+    });
+  }
+
+  override async link(version: string): Promise<void> {
+    const src = join(this.pathSvc.versionedToolPath(this.name, version), 'bin');
+
+    await this.shellwrapper({ srcDir: src });
+  }
+
+  override async test(_version: string): Promise<void> {
+    await execa(this.name, ['--version'], {
+      stdio: ['inherit', 'inherit', 1],
+    });
+  }
+}
