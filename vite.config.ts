a6d5f38d2a222daf10e7ec1130ad9539627e3a2b
Michael Kriese <michael.kriese@visualon.de>
2025-02-26 13:28:01 +0100

    test: use builtin github reporter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { env } from 'node:process';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
-import GitHubActionsReporter from 'vitest-github-actions-reporter';
 
 const ci = !!env.CI;
 
@@ -9,12 +8,11 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     coverage: {
+      provider: 'v8',
       reporter: ci ? ['lcovonly', 'text'] : ['html', 'text'],
       include: ['src/cli/**/*.ts', '!**/__mocks__/**', '!**/types.ts'],
     },
-    reporters: ci
-      ? ['default', new GitHubActionsReporter()]
-      : ['default', 'html'],
+    reporters: ci ? ['default', 'github-actions'] : ['default', 'html'],
     restoreMocks: true,
     setupFiles: './test/global-setup.ts',
     deps: { moduleDirectories: ['node_modules', '.yarn/'] },
