214129f5306b73e443888271508a3fcd093a243f
Michael Kriese <michael.kriese@visualon.de>
2025-09-05 13:40:43 +0200

    test: add tests

--- src/cli/install-tool/index.spec.ts
+++ src/cli/install-tool/index.spec.ts
@@ -1,6 +1,15 @@
 import fs from 'node:fs/promises';
 import { beforeAll, describe, expect, test, vi } from 'vitest';
 import { rootPath } from '../../../test/path';
+import { VersionService } from '../services';
+import { NpmVersionResolver } from '../tools/node/resolver';
+import { NpmBaseInstallService } from '../tools/node/utils';
+import { PipVersionResolver } from '../tools/python/pip';
+import { PipBaseInstallService } from '../tools/python/utils';
+import {
+  RubyBaseInstallService,
+  RubyGemVersionResolver,
+} from '../tools/ruby/utils';
 import { installTool, resolveVersion } from '.';
 
 vi.mock('del');
@@ -21,12 +30,58 @@ describe('cli/install-tool/index', () => {
     }
   });
 
-  test('installTool', async () => {
-    expect(await installTool('bun', '1.0.0')).toBeUndefined();
-    expect(await installTool('dummy', '1.0.0')).toBeUndefined();
+  describe('installTool', () => {
+    test('works', async () => {
+      expect(await installTool('bun', '1.0.0')).toBeUndefined();
+      expect(await installTool('dummy', '1.0.0')).toBeUndefined();
+    });
+
+    test.each([
+      {
+        type: 'gem' as const,
+        svc: RubyBaseInstallService,
+      },
+      {
+        type: 'npm' as const,
+        svc: NpmBaseInstallService,
+      },
+      {
+        type: 'pip' as const,
+        svc: PipBaseInstallService,
+      },
+    ])('works: $type', async ({ type, svc }) => {
+      vi.spyOn(svc.prototype, 'install').mockResolvedValue();
+      vi.spyOn(svc.prototype, 'needsInitialize').mockReturnValue(false);
+      vi.spyOn(svc.prototype, 'isInstalled').mockResolvedValue(false);
+      vi.spyOn(svc.prototype, 'validate').mockResolvedValue(true);
+      vi.spyOn(svc.prototype, 'postInstall').mockResolvedValue();
+      vi.spyOn(svc.prototype, 'test').mockRejectedValue(new Error('test'));
+      vi.spyOn(VersionService.prototype, 'find').mockResolvedValue(null);
+      expect(await installTool('dummy', '1.0.0', false, type)).toBeUndefined();
+    });
   });
 
-  test('resolveVersion', async () => {
-    expect(await resolveVersion('composer', '1.0.0')).toBe('1.0.0');
+  describe('resolveVersion', () => {
+    test('works', async () => {
+      expect(await resolveVersion('composer', '1.0.0')).toBe('1.0.0');
+    });
+
+    test.each([
+      {
+        type: 'gem' as const,
+        svc: RubyGemVersionResolver,
+      },
+      {
+        type: 'npm' as const,
+        svc: NpmVersionResolver,
+      },
+      {
+        type: 'pip' as const,
+        svc: PipVersionResolver,
+      },
+    ])('works: $type', async ({ type, svc }) => {
+      vi.spyOn(svc.prototype, 'resolve').mockResolvedValue('1.0.0');
+      expect(await resolveVersion('dummy', '1.0.0', type)).toBe('1.0.0');
+    });
   });
 });
