e095549bbf16f4c3a5cb4205b0c69ece99e094df
Michael Kriese <michael.kriese@visualon.de>
2026-01-27 13:36:43 +0100

    fix: call `process.exit` to force shutdown (#5820)
    (cherry picked from commit c055fb88353e276bafa7821bcb6674ee1bd0fc3a)

--- src/cli/main.spec.ts
+++ src/cli/main.spec.ts
@@ -4,6 +4,7 @@ import { main } from './main';
 const mocks = vi.hoisted(() => ({
   argv0: 'containerbase-cli',
   argv: ['node', 'containerbase-cli', 'help'],
+  exit: vi.fn(),
 }));
 
 vi.mock('node:process', async (importOriginal) => ({
@@ -20,5 +21,6 @@ describe('cli/main', () => {
   test('works', async () => {
     vi.spyOn(process.stdout, 'write').mockReturnValue(true);
     expect(await main()).toBeUndefined();
+    expect(mocks.exit).toHaveBeenCalled();
   });
 });
