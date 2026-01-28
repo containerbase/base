ea2098acd1db50579b440acd342af56d89b24af5
Michael Kriese <michael.kriese@visualon.de>
2026-01-28 10:27:39 +0100

    build: fix docker tags on maintenanve branches

--- tools/prepare-release.js
+++ tools/prepare-release.js
@@ -7,27 +7,32 @@ shell.config.fatal = true;
 
 class PrepareCommand extends Command {
   release = Option.String('-r,--release', { required: true });
+  channel = Option.String('-c,--channel', { required: true });
   gitSha = Option.String('--sha');
   dryRun = Option.Boolean('-d,--dry-run');
 
   async execute() {
-    const version = this.release;
+    const { channel, dryRun, gitSha, release } = this;
 
-    shell.echo(`Preparing version: ${version}`);
+    shell.echo(`Preparing version: ${release} (${gitSha}, ${channel})`);
 
-    if (this.dryRun) {
+    if (dryRun) {
       shell.echo('DRY-RUN: done.');
       return 0;
     }
 
-    process.env.TAG = version;
-    process.env.CONTAINERBASE_VERSION = version;
+    process.env.TAG = release;
+    process.env.CONTAINERBASE_VERSION = release;
+
+    if (channel) {
+      process.env.CHANNEL = channel.replace('maint/', '');
+    }
 
     shell.mkdir('-p', 'bin');
 
     shell.exec('pnpm build');
 
-    await fs.writeFile('dist/docker/usr/local/containerbase/version', version);
+    await fs.writeFile('dist/docker/usr/local/containerbase/version', release);
     shell.exec(`tar -cJf ./bin/containerbase.tar.xz -C ./dist/docker .`);
 
     await hashFile('./bin/containerbase.tar.xz', 'sha512');
