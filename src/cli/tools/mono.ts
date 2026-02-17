2fbc11dba192384ed8f270e4cb06e2d475bae9c2
Michael Kriese <michael.kriese@visualon.de>
2026-02-17 10:13:31 +0100

    feat: add nuget tool (#5992)
    * feat: add nuget tool
    
    * fix: update import
    
    * fix: wrong url
    
    * fix(mono): init ssl certs

--- src/cli/tools/mono.ts
+++ src/cli/tools/mono.ts
@@ -1,8 +1,9 @@
 import fs from 'fs/promises';
 import { join } from 'node:path';
+import { codeBlock } from 'common-tags';
 import { injectFromHierarchy, injectable } from 'inversify';
 import { BasePrepareService } from '../prepare-tool/base-prepare.service';
-import { pathExists } from '../utils';
+import { logger, pathExists } from '../utils';
 import {
   PrebuildInstallService,
   PrebuildVersionResolver,
@@ -20,6 +21,43 @@ export class MonoPrepareService extends BasePrepareService {
     if (!(await pathExists(mono))) {
       await fs.symlink(join(this.pathSvc.cachePath, '.mono'), mono);
     }
+
+    const src = join(this.pathSvc.sslPath, 'mono');
+    if (!(await pathExists(src))) {
+      await this.pathSvc.createDir(src);
+      // inspired by debian mono packages
+      const certSync = join(this.pathSvc.binDir, 'cert-sync');
+      await fs.writeFile(
+        join(
+          this.envSvc.rootDir,
+          'etc/ca-certificates/update.d/containerbase-mono-keystore',
+        ),
+        codeBlock`
+          #!/bin/sh
+
+          set -e
+
+          [ -f ${certSync} ] || exit 0
+
+          echo "Updating Mono key store"
+
+          # always return true, even if the tool blows up for some reason.
+          # we don't want to mess with users at install-time. failure here
+          # isn't really fatal anyway, just inconvenient
+          ${certSync} /etc/ssl/certs/ca-certificates.crt || true
+
+          echo "Done"
+        `,
+        { mode: this.envSvc.umask },
+      );
+    }
+
+    const tgt = join(this.envSvc.rootDir, `usr/share/.mono`);
+    if (await pathExists(tgt)) {
+      logger.warn(`Removing existing mono link at ${tgt}`);
+      await fs.rm(tgt);
+    }
+    await fs.symlink(src, tgt);
   }
 
   override async initialize(): Promise<void> {
@@ -34,6 +72,38 @@ export class MonoPrepareService extends BasePrepareService {
 @injectFromHierarchy()
 export class MonoInstallService extends PrebuildInstallService {
   readonly name = 'mono';
+
+  override async install(version: string): Promise<void> {
+    await super.install(version);
+    // create cert-sync wrapper to create the mono cert store
+    const path = this.pathSvc.versionedToolPath(this.name, version);
+    if (!(await pathExists(join(path, 'bin/cert-sync')))) {
+      await fs.writeFile(
+        join(path, 'bin/cert-sync'),
+        codeBlock`
+        #!/bin/sh
+        ${path}/bin/mono ${path}/lib/mono/4.5/cert-sync.exe "$@"
+      `,
+        {
+          mode: this.envSvc.umask,
+        },
+      );
+    }
+
+    if (
+      !(await pathExists(join(this.pathSvc.sslPath, 'mono/new-certs/Trust')))
+    ) {
+      await this._spawn(path + '/bin/cert-sync', [
+        '/etc/ssl/certs/ca-certificates.crt',
+      ]);
+    }
+  }
+
+  override async link(version: string): Promise<void> {
+    await super.link(version);
+    const src = join(this.pathSvc.versionedToolPath(this.name, version), 'bin');
+    await this.shellwrapper({ srcDir: src, name: 'cert-sync' });
+  }
 }
 
 @injectable()
---
+++ src/cli/tools/nuget.ts
@@ -0,0 +1,75 @@
+import fs from 'node:fs/promises';
+import { join } from 'node:path';
+import { isNonEmptyStringAndNotWhitespace } from '@sindresorhus/is';
+import { injectFromHierarchy, injectable } from 'inversify';
+import { z } from 'zod';
+import { BaseInstallService } from '../install-tool/base-install.service';
+import { ToolVersionResolver } from '../install-tool/tool-version-resolver';
+
+@injectable()
+@injectFromHierarchy()
+export class NugetInstallService extends BaseInstallService {
+  readonly name = 'nuget';
+  override parent = 'mono';
+
+  override async install(version: string): Promise<void> {
+    const baseUrl = `https://dist.nuget.org/win-x86-commandline/v${version}/`;
+    const filename = `${this.name}.exe`;
+
+    const file = await this.http.download({
+      url: `${baseUrl}${filename}`,
+      fileName: `${filename}-v${version}`,
+    });
+
+    await this.pathSvc.ensureToolPath(this.name);
+
+    const path = join(
+      await this.pathSvc.createVersionedToolPath(this.name, version),
+      'bin',
+    );
+    await fs.mkdir(path);
+    const binary = join(path, filename);
+    await fs.copyFile(file, binary);
+    // create shell wrapper to be able to execute it with mono
+    const wrapper = join(path, this.name);
+    await fs.writeFile(wrapper, `#!/bin/sh\nexec mono "${binary}" "$@"\n`);
+    await fs.chmod(wrapper, this.envSvc.umask);
+  }
+
+  override async link(version: string): Promise<void> {
+    const src = join(this.pathSvc.versionedToolPath(this.name, version), 'bin');
+
+    await this.shellwrapper({ srcDir: src });
+  }
+
+  override async test(_version: string): Promise<void> {
+    await this._spawn(this.name, ['help']);
+  }
+}
+
+const NugetVersion = z.object({
+  version: z.string(),
+  stage: z.string().optional(),
+});
+
+const NugetTools = z.object({
+  'nuget.exe': z.array(NugetVersion),
+});
+
+@injectable()
+@injectFromHierarchy()
+export class NugetVersionResolver extends ToolVersionResolver {
+  readonly tool = 'nuget';
+
+  async resolve(version: string | undefined): Promise<string | undefined> {
+    if (!isNonEmptyStringAndNotWhitespace(version) || version === 'latest') {
+      const meta = NugetTools.parse(
+        await this.http.getJson('https://dist.nuget.org/tools.json'),
+      );
+      // we know that the latest version is the first entry, so search for first lts
+      return meta['nuget.exe'].find((v) => v.stage === 'ReleasedAndBlessed')
+        ?.version;
+    }
+    return version;
+  }
+}
